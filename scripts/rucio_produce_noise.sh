79eab046a8d91c3dafdf75b2e4f2dec42a661931
Riccardo Di Maria <riccardo.di.maria@cern.ch>
2020-10-02 19:30:15 +0200

    Generalising RUCIO Produce Noise

--- scripts/rucio_produce_noise.sh
+++ scripts/rucio_produce_noise.sh
@@ -1,39 +1,37 @@
 #!/bin/bash
 
-rses[0]="ALPAMED-DPM"
-rses[1]="CNAF-STORM"
-rses[2]="DESY-DCACHE"
-rses[3]="EULAKE-1"
-rses[4]="EULAKE-2"
-rses[5]="GSI-ROOT"
-rses[6]="IN2P3-CC-DCACHE"
-rses[7]="INFN-NA-DPM"
-rses[8]="LAPP-DCACHE"
-rses[9]="LAPP-DPM"
-rses[10]="PIC-DCACHE"
-rses[11]="PIC-INJECT"
-rses[12]="SARA-DCACHE"
+python Utilities-and-Operations-Scripts/cric-info-tools/list_rses_from_cric.py -o list_rses_from_cric.txt
+
+rses=()
+while read line
+do
+    rses+=($line)
+done < list_rses_from_cric.txt
+
+len=${#rses[@]}
 
 echo '* RUCIO Produce Noise * Exporting ENV Variables'
 
-SLEEP_TIME_MINUTES=${NOISE_SLEEP_TIME_MINUTES:-20m}
+SLEEP_TIME_MINUTES=${NOISE_SLEEP_TIME_MINUTES:-30m}
 FILE_SIZE=${FILE_SIZE:-10M}
+RUCIO_SCOPE=${RUCIO_SCOPE:-ESCAPE_CERN_TEAM-noise}
 
 upload_and_transfer () {
-    len=${#rses[@]}
     for (( i=0; i<$len; i++ )); do
         RANDOM_STRING=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
         
         filename=/tmp/auto_uploaded_$RANDOM_STRING
         did=auto_uploaded_$RANDOM_STRING
         
-        echo $filename
+        echo '*** filename' $filename
         
         truncate -s $FILE_SIZE $filename
         
         if [ $1 != $i ]; then
-	        rucio upload --rse ${rses[$1]} --scope noise_5 $filename || return '1' #continue
-            rucio add-rule noise_5:$did 1 ${rses[$i]};
+            echo '*** upload to rse' ${rses[$1]}
+	        rucio upload --rse ${rses[$1]} --scope $RUCIO_SCOPE $filename || return '1' #continue
+            echo '*** add-rule to rse' ${rses[$i]}
+            rucio add-rule $RUCIO_SCOPE:$did 1 ${rses[$i]};
 	    fi
         rm $filename
     done
@@ -42,20 +40,9 @@ upload_and_transfer () {
 echo '* RUCIO Produce Noise * Initializing Loop'
 
 while true; do
-
-    upload_and_transfer 0
-    upload_and_transfer 1
-    upload_and_transfer 2
-    upload_and_transfer 3
-    upload_and_transfer 4
-    upload_and_transfer 5
-    upload_and_transfer 6
-    upload_and_transfer 7
-    upload_and_transfer 8
-    upload_and_transfer 9
-    upload_and_transfer 10
-    upload_and_transfer 11
-    upload_and_transfer 12
+    for (( j=0; j<$len; j++ )); do
+        upload_and_transfer $j
+    done
 
     echo '* RUCIO Produce Noise * Will sleep for ' $SLEEP_TIME_MINUTES
     sleep $SLEEP_TIME_MINUTES
