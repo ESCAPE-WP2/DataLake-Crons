206407ec62f4d22a0e58fc09835c2d0401344aa9
Rizart Dona <rizart.dona@gmail.com>
2020-10-26 16:41:41 +0100

    run FTS tests on background

--- scripts/fts_datalake_test.sh
+++ scripts/fts_datalake_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SLEEP_TIME_MINUTES=${FTS_DATALAKE_TEST_SLEEP_TIME_MINUTES:-10m}
+SLEEP_TIME_MINUTES=${FTS_DATALAKE_TEST_SLEEP_TIME_MINUTES:-30m}
 
 echo '* FTS Testing * Exporting ENV Variables (X509: ' $X509_USER_PROXY ' )'
 
@@ -11,11 +11,15 @@ export FTS_LOCALPATH="/scripts/temp_files_fts/"
 
 echo '* FTS Testing * Initializing Loop'
 
+fts_testing(){
+    python fts_datalake_test.py -i conf/datalake.json --cleanup --exit
+    python fts_datalake_test.py -i conf/lapp_webdav.json
+    python fts_datalake_test.py -i conf/datalake.json
+}
+
 while true; do
     cd /scripts/fts-analysis-datalake/
-    python fts_datalake_test.py -i conf/datalake.json --cleanup --exit
-    python fts_datalake_test.py -i conf/datalake.json
-    python fts_datalake_test.py -i conf/lapp_webdav.json
+    fts_testing &
     echo '* FTS Testing * Will sleep for ' $SLEEP_TIME_MINUTES
     sleep $SLEEP_TIME_MINUTES
 done
