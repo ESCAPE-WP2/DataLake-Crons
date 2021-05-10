2e1b7a689598e240cd765ace39d1bec61253e603
Rizart Dona <rizart.dona@gmail.com>
2021-05-10 11:00:26 +0200

    enable Functional Test activity for Rucio test rules

--- scripts/rucio_produce_noise.sh
+++ scripts/rucio_produce_noise.sh
@@ -37,7 +37,7 @@ upload_and_transfer () {
             echo '*** upload to rse' ${rses[$1]}
             rucio -v upload --rse ${rses[$1]} --lifetime $FILE_LIFETIME --scope $RUCIO_SCOPE $filename || return '1' #continue
             echo '*** add-rule to rse' ${rses[$i]}
-            rucio add-rule --lifetime $FILE_LIFETIME $RUCIO_SCOPE:$did 1 ${rses[$i]};
+            rucio add-rule --lifetime $FILE_LIFETIME --activity "Functional Test" $RUCIO_SCOPE:$did 1 ${rses[$i]};
 	    fi
         rm $filename
     done
